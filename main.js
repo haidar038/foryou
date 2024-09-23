912684a552a3374143e650ec852939ebad7c2a6e
M. Khaidar <haidar038@gmail.com>
2024-09-24 02:44:15 +0900

    Changes to be committed: 	new file:   diego-ph-5LOhydOtTKU-unsplash.jpg 	new file:   index.html 	new file:   main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+const wrapper = document.getElementById("wrapper");
+const lidOne = document.getElementById("lidOne");
+const lidTwo = document.getElementById("lidTwo");
+const letter = document.getElementById("letter");
